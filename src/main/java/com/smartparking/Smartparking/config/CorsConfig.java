f3975ea00c876e12541be0765b9fa1f38891506d
Carlos Chávez <89101032+CarlosChavez19@users.noreply.github.com>
2025-11-29 16:31:26 -0500

    Update CorsConfig.java

--- src/main/java/com/smartparking/Smartparking/config/CorsConfig.java
+++ src/main/java/com/smartparking/Smartparking/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://smart-parking-iot-3475.netlify.app/")
+                        .allowedOrigins("https://smart-parking-iot-3475.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                         .allowedHeaders("*")
                         .exposedHeaders("*")
